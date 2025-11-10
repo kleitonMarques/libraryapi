eb1d7f804ce309c604b13629857469d493d87db9
Kleiton Marques <kleitonm.marques@gmail.com>
2025-11-10 08:07:42 -0300

    Working with OneToMany relationships

--- src/test/java/io/github/kleitonmarques/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/kleitonmarques/libraryapi/repository/AutorRepositoryTest.java
@@ -1,11 +1,15 @@
 package io.github.kleitonmarques.libraryapi.repository;
 
 import io.github.kleitonmarques.libraryapi.model.Autor;
+import io.github.kleitonmarques.libraryapi.model.GeneroLivro;
+import io.github.kleitonmarques.libraryapi.model.Livro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +19,9 @@ public class AutorRepositoryTest {
 
     @Autowired
     AutorRepository repository;
+
+    @Autowired
+    LivroRepository livroRepository;
 
     @Test
     public void salvarTest() {
@@ -63,5 +70,36 @@ public class AutorRepositoryTest {
         var id = UUID.fromString("0d55f1ea-e670-4d0d-923c-2e0bf1716c2f");
         var maria = repository.findById(id).get();
         repository.delete(maria);
+    }
+
+    @Test
+    void salvarAutorComLivrosTest() {
+        Autor autor = new Autor();
+        autor.setNome("Antonio");
+        autor.setNacionalidade("Americano");
+        autor.setDataNascimento(LocalDate.of(1970, 8, 5));
+
+        Livro livro = new Livro();
+        livro.setIsbn("20847-84874");
+        livro.setPreco(BigDecimal.valueOf(204));
+        livro.setGenero(GeneroLivro.MISTERIO);
+        livro.setTitulo("O roubo da casa assombrada");
+        livro.setDataPublicacao(LocalDate.of(1999, 1, 2));
+        livro.setAutor(autor);
+
+        Livro livro2 = new Livro();
+        livro2.setIsbn("99999-84874");
+        livro2.setPreco(BigDecimal.valueOf(650));
+        livro2.setGenero(GeneroLivro.MISTERIO);
+        livro2.setTitulo("O roubo da casa assombrada");
+        livro2.setDataPublicacao(LocalDate.of(2000, 1, 2));
+        livro2.setAutor(autor);
+
+        autor.setLivros(new ArrayList<>());
+        autor.getLivros().add(livro);
+        autor.getLivros().add(livro2);
+
+        repository.save(autor);
+//        livroRepository.saveAll(autor.getLivros());
     }
 }
