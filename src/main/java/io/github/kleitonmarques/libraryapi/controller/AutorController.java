a1574db6a1595f53139739afb438168fcc636d62
Kleiton Marques <kleitonm.marques@gmail.com>
2025-12-22 08:04:35 -0300

    Starting API coding

---
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/AutorController.java
@@ -0,0 +1,10 @@
+package io.github.kleitonmarques.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autores")
+// http://localhost:8080/autores
+public class AutorController {
+}
