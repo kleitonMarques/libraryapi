3442006cf2efaa65ca840929cbc6c318120ac825
Kleiton Marques <kleitonm.marques@gmail.com>
2026-01-07 07:31:11 -0300

    Author search with filter

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/AutorDTO.java
@@ -3,8 +3,14 @@ package io.github.kleitonmarques.libraryapi.controller.dto;
 import io.github.kleitonmarques.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record AutorDTO(String nome, LocalDate dataNascimento, String nacionalidade) {
+public record AutorDTO(
+        UUID id,
+        String nome,
+        LocalDate dataNascimento,
+        String nacionalidade
+) {
 
     public Autor mapearParaAutor() {
         Autor autor = new Autor();
