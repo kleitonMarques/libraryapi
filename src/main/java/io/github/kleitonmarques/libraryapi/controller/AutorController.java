6df05d0c8e51330fe1ff2a029a0fbf97c8cf3510
Kleiton Marques <kleitonm.marques@gmail.com>
2026-01-21 20:28:20 -0300

    Adding field validation with Bean Validation

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/AutorController.java
@@ -6,6 +6,7 @@ import io.github.kleitonmarques.libraryapi.exceptions.OperacaoNaoPermitidaExcept
 import io.github.kleitonmarques.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.kleitonmarques.libraryapi.model.Autor;
 import io.github.kleitonmarques.libraryapi.service.AutorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +28,7 @@ public class AutorController {
     private final AutorService service;
 
     @PostMapping
-    public ResponseEntity<Object> salvar(@RequestBody AutorDTO autor) {
+    public ResponseEntity<Object> salvar(@RequestBody @Valid AutorDTO autor) {
         try {
             Autor autorEntidade = autor.mapearParaAutor();
             service.salvar(autorEntidade);
