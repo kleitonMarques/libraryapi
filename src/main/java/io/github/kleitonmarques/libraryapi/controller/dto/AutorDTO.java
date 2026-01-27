395a39cd5765555b4e3bd27764f7341b220e2a02
Kleiton Marques <kleitonm.marques@gmail.com>
2026-01-26 21:18:14 -0300

    Exploring validation types in Spring

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/AutorDTO.java
@@ -3,6 +3,8 @@ package io.github.kleitonmarques.libraryapi.controller.dto;
 import io.github.kleitonmarques.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,10 +12,13 @@ import java.util.UUID;
 public record AutorDTO(
         UUID id,
         @NotBlank(message = "campo obrigatório")
+        @Size(min = 2, max = 100, message = "Campo fora do tamanho padrão")
         String nome,
         @NotNull(message = "campo obrigatório")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
         @NotBlank(message = "campo obrigatório")
+        @Size(min = 2, max = 100, message = "Campo fora do tamanho padrão")
         String nacionalidade
 ) {
 
