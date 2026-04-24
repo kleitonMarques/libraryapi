8f8137747fedcd7604f62217c2af5de02753f850
Kleiton Marques <kleitonm.marques@gmail.com>
2026-04-23 21:03:24 -0300

    Endpoint para cadastro de livro com Bean Validation

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/LivroController.java
@@ -1,7 +1,14 @@
 package io.github.kleitonmarques.libraryapi.controller;
 
+import io.github.kleitonmarques.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.kleitonmarques.libraryapi.controller.dto.ErroResposta;
+import io.github.kleitonmarques.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.kleitonmarques.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
     private final LivroService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
+        try {
+            // mapear dto para entidade
+            // enviar a entidade para o service validar e salvar na base
+            // criar url para acesso dos dados do livro
+            // retornar codigo created com header location
+            return ResponseEntity.ok(dto);
+        } catch (RegistroDuplicadoException e) {
+            var erroDto = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDto.status()).body(erroDto);
+        }
+    }
 }
--- src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,17 +1,27 @@
 package io.github.kleitonmarques.libraryapi.controller.dto;
 
 import io.github.kleitonmarques.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
+            @ISBN
+            @NotBlank(message = "campo obrigatório")
             String isbn,
+            @NotBlank(message = "campo obrigatório")
             String titulo,
+            @NotNull(message = "campo obrigatório")
+            @Past(message = "não pode ser uma data futura")
             LocalDate dataPublicacao,
             GeneroLivro genero,
             BigDecimal preco,
+            @NotNull(message = "campo obrigatório")
             UUID idAutor
 ) {
 }
