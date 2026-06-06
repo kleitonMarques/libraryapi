2992cf2faaf6c785e5ee0e7a27e555427c02109a
Kleiton Marques <kleitonm.marques@gmail.com>
2026-06-06 14:38:40 -0300

    Implementando as Specifications da entidade Livro

--- src/main/java/io/github/kleitonmarques/libraryapi/service/LivroService.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/service/LivroService.java
@@ -3,6 +3,7 @@ package io.github.kleitonmarques.libraryapi.service;
 import io.github.kleitonmarques.libraryapi.model.GeneroLivro;
 import io.github.kleitonmarques.libraryapi.model.Livro;
 import io.github.kleitonmarques.libraryapi.repository.LivroRepository;
+import io.github.kleitonmarques.libraryapi.repository.specs.LivroSpecs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.github.kleitonmarques.libraryapi.repository.specs.LivroSpecs.*;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +32,29 @@ public class LivroService {
         repository.delete(livro);
     }
 
-    public List<Livro> pesquisa(String isbn, String nomeAutor, GeneroLivro genero, Integer anoPublicacao) {
+    public List<Livro> pesquisa(String isbn, String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicacao) {
+        // select * from livro where isbn = :isbn and nomeAutor =
+        // Specification <Livro> specs = Specification
+        //  .where(LivroSpecs.isbnEqual(isbn))
+        //  .and(LivroSpecs.tituloLike(titulo))
+        //  .and(LivroSpecs.generoEqual(genero));
 
-        Specification <Livro> specs = null;
+        // select * from livro where 0 = 0
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if (isbn != null) {
+            // query = query and isbn = :isbn
+            specs = specs.and(isbnEqual(isbn));
+        }
+
+        if (titulo != null) {
+            specs = specs.and(tituloLike(titulo));
+        }
+
+        if (genero != null) {
+            specs = specs.and(generoEqual(genero));
+        }
+
         return repository.findAll(specs);
     }
 }
