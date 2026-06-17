bb2d09ba64a2cdbaaa767ea0693e3fe6f49fc67a
Kleiton Marques <kleitonm.marques@gmail.com>
2026-06-17 20:06:52 -0300

    Validação de ano da data da publicação

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/common/GlobalExceptionHandler.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package io.github.kleitonmarques.libraryapi.controller.common;
 
 import io.github.kleitonmarques.libraryapi.controller.dto.ErroCampo;
 import io.github.kleitonmarques.libraryapi.controller.dto.ErroResposta;
+import io.github.kleitonmarques.libraryapi.exceptions.CampoInvalidoException;
 import io.github.kleitonmarques.libraryapi.exceptions.OperacaoNaoPermitidaException;
 import io.github.kleitonmarques.libraryapi.exceptions.RegistroDuplicadoException;
 import org.springframework.http.HttpStatus;
@@ -44,6 +45,15 @@ public class GlobalExceptionHandler {
     public ErroResposta handleOperacaoNaoPermitidaException(
             OperacaoNaoPermitidaException e) {
         return ErroResposta.respostaPadrao(e.getMessage());
+    }
+
+    @ExceptionHandler(CampoInvalidoException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleCampoInvalidoException(CampoInvalidoException e) {
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação.",
+                List.of(new ErroCampo(e.getCampo(), e.getMessage())));
     }
 
     @ExceptionHandler(RuntimeException.class)
