26ce8c529e0a3a9018d5054208d165b13e25e163
Kleiton Marques <kleitonm.marques@gmail.com>
2026-07-12 12:23:49 -0300

    Criando um UserDetailsService Customizado

--- src/main/java/io/github/kleitonmarques/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.kleitonmarques.libraryapi.config;
 
+import io.github.kleitonmarques.libraryapi.security.CustomUserDetailsService;
+import io.github.kleitonmarques.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -44,19 +46,20 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails user1 = User.builder()
-                .username("usuario")
-                .password(encoder.encode("123"))
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password(encoder.encode("321"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
+//        UserDetails user1 = User.builder()
+//                .username("usuario")
+//                .password(encoder.encode("123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("321"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+        return new CustomUserDetailsService(usuarioService);
     }
 }
