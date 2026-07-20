b1c2a2d0ce10e5a9f38cd907526353e45f338bd3
Kleiton Marques <kleitonm.marques@gmail.com>
2026-07-20 20:24:53 -0300

    Criando estratégia para unificar o usuário do Google

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/LoginViewController.java
@@ -1,5 +1,6 @@
 package io.github.kleitonmarques.libraryapi.controller;
 
+import io.github.kleitonmarques.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,9 @@ public class LoginViewController {
     @GetMapping("/")
     @ResponseBody
     public String paginaHome(Authentication authentication) {
+        if(authentication instanceof CustomAuthentication customAlth) {
+            System.out.println(customAlth.getUsuario());
+        }
         return "Olá " + authentication.getName();
     }
 }
