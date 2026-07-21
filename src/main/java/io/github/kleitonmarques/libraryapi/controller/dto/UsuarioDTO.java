7cc8331580487bbb0ddef4efd15d0afe5f44fed2
Kleiton Marques <kleitonm.marques@gmail.com>
2026-07-21 20:30:43 -0300

    Adicionando campo para salvar email no cadastro de usuários

--- src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/UsuarioDTO.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/controller/dto/UsuarioDTO.java
@@ -1,6 +1,17 @@
 package io.github.kleitonmarques.libraryapi.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
-public record UsuarioDTO(String login, String senha, List<String> roles) {
+public record UsuarioDTO(
+        @NotBlank(message = "campo obrigatório")
+        String login,
+        @Email (message = "inválido")
+        @NotBlank(message = "campo obrigatório")
+        String email,
+        @NotBlank(message = "campo obrigatório")
+        String senha,
+        List<String> roles) {
 }
