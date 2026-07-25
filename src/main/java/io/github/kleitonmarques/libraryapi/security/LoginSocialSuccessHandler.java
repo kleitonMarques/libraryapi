e83c33d86fb147f4990a4e7a8eb83681b8a2302f
Kleiton Marques <kleitonm.marques@gmail.com>
2026-07-24 21:08:09 -0300

    Lógica para novos usuários que só tem login Google

--- src/main/java/io/github/kleitonmarques/libraryapi/security/LoginSocialSuccessHandler.java
+++ src/main/java/io/github/kleitonmarques/libraryapi/security/LoginSocialSuccessHandler.java
@@ -14,10 +14,13 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String SENHA_PADRAO = "321";
 
     private final UsuarioService usuarioService;
 
@@ -25,19 +28,36 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
     public void onAuthenticationSuccess(
             HttpServletRequest request,
             HttpServletResponse response,
-            Authentication authentication) throws ServletException, IOException {
+            Authentication authentication
+        ) throws ServletException, IOException {
 
         OAuth2AuthenticationToken auth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
         OAuth2User oAuth2User = auth2AuthenticationToken.getPrincipal();
-
         String email = oAuth2User.getAttribute("email");
-
         Usuario usuario = usuarioService.obterPorEmail(email);
 
+        if(usuario == null) {
+            usuario = cadastrarUsuarioNaBase(email);
+        }
+
         authentication = new CustomAuthentication(usuario);
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    private Usuario cadastrarUsuarioNaBase(String email) {
+        Usuario usuario;
+        usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setLogin(obterLoginApartirEmail(email));
+        usuario.setSenha(SENHA_PADRAO);
+        usuario.setRoles(List.of("OPERADOR"));
+
+        usuarioService.salvar(usuario);
+        return usuario;
+    }
+
+    private String obterLoginApartirEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
     }
 }
